9e868c92fb04bb1658eb10277736dacdfef47d73
sdgandhi <sdgandhi@users.noreply.github.com>
2020-03-02 00:56:07 -0800

    Update privacy

--- src/Privacy.js
+++ src/Privacy.js
@@ -9,7 +9,7 @@ class Privacy extends Component {
 					<Col>
 						<h1>Privacy Policy</h1>
 
-						<p>catchup operates the catchup website, which provides the the Arra app.</p>
+						<p>catchup operates the catchup website, which provides the the related app, website and services.</p>
 
 						<p>This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the catchup website.</p>
 
